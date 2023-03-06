4fffbe1005e1cd3fcbec28680848bd1c83cb20ef
SMpofana <53563133+SMpofana@users.noreply.github.com>
2023-03-06 23:33:25 +0300

    Add files via upload

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -5,10 +5,11 @@ var menuButton = document.querySelector('.hamburger_lines');
 var menuButtonLines = Array.from(menuButton.children);
 var menuList = document.querySelector(".main_menu");
 var menuItems = document.querySelectorAll('.menu_item');
+var menu = Array.from(menuItems);
 var menuCta = document.querySelector('.header_cta');
 var menuCtaButton = menuCta.firstElementChild;
 var menuWord = document.getElementById('menu-word');
-console.log(menuItems);
+console.log(menu);
 menuButton.addEventListener('click', function (e) {
   menuList.classList.toggle('active');
   menuCtaButton.classList.toggle('active');
@@ -21,4 +22,19 @@ menuButton.addEventListener('click', function (e) {
   } else {
     menuWord.innerHTML = 'menu';
   }
+});
+menu.forEach(function (item) {
+  item.addEventListener('click', function (e) {
+    menuList.classList.toggle('active');
+    menuCtaButton.classList.toggle('active');
+    menuButtonLines.forEach(function (item) {
+      item.classList.toggle('active');
+    });
+
+    if (menuWord.innerHTML === 'menu') {
+      menuWord.innerHTML = 'close';
+    } else {
+      menuWord.innerHTML = 'menu';
+    }
+  });
 });
