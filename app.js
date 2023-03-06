500705bfd4833c476faec9e69bc394192e2ad50a
SMpofana <53563133+SMpofana@users.noreply.github.com>
2023-03-06 23:03:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const menuButton = document.querySelector('.hamburger_lines')
 const menuButtonLines = Array.from(menuButton.children);
 const menuList = document.querySelector(".main_menu")
 const menuItems = document.querySelectorAll('.menu_item');
-const menuCta = document.querySelector('.header_cta')
-const menuCtaButton = menuCta.firstElementChild
-const menuWord = document.getElementById('menu-word')
+const menu = Array.from(menuItems)
+const menuCta = document.querySelector('.header_cta');
+const menuCtaButton = menuCta.firstElementChild;
+const menuWord = document.getElementById('menu-word');
 
 
-console.log(menuItems)
+
+console.log(menu)
 
 menuButton.addEventListener('click', e=>{
     
@@ -24,5 +26,25 @@ menuButton.addEventListener('click', e=>{
     }else{
         menuWord.innerHTML = 'menu'
     }
+
+})
+
+menu.forEach((item)=>{
+
+    item.addEventListener('click', e=>{
+
+        menuList.classList.toggle('active');
+        menuCtaButton.classList.toggle('active');
+
+        menuButtonLines.forEach((item)=>{
+            item.classList.toggle('active')
+        })
+
+        if(menuWord.innerHTML === 'menu'){
+            menuWord.innerHTML = 'close';
+        }else{
+            menuWord.innerHTML = 'menu'
+        }
+        })
 
 })
